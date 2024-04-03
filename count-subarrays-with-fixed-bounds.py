3bf267fe120a980e2165a34bcbe96dd26c65ea4e
Mariana Gonzalez Bejar <mgonza32@nd.edu>
2024-04-03 13:23:10 -0400

    Update count-subarrays-with-fixed-bounds.py

--- count-subarrays-with-fixed-bounds.py
+++ count-subarrays-with-fixed-bounds.py
@@ -1,4 +1,30 @@
+'''
+You are given an integer array nums and two integers minK and maxK.
 
+A fixed-bound subarray of nums is a subarray that satisfies the following conditions:
+- The minimum value in the subarray is equal to minK.
+- The maximum value in the subarray is equal to maxK.
+- Return the number of fixed-bound subarrays.
+
+A subarray is a contiguous part of an array.
+
+Example 1:
+Input: nums = [1,3,5,2,7,5], minK = 1, maxK = 5
+Output: 2
+Explanation: The fixed-bound subarrays are [1,3,5] and [1,3,5,2].
+
+Example 2:
+Input: nums = [1,1,1,1], minK = 1, maxK = 1
+Output: 10
+Explanation: Every subarray of nums is a fixed-bound subarray. There are 10 possible subarrays.
+
+Runtime: 644 ms
+Memory: 31.02 MB
+
+Complexity: 
+Time complexity: The code has a for loop through the array with constant-time operations for each element. Creating a time complexity of O(n), where n is the length of the array.
+Space complexity: The code uses a fixed amount of extra space for pointers and count variable. Creating a space complexity of O(1), which is independent of the input size.
+'''
 
 class Solution:
     def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
